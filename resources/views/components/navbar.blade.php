0c8555f75bdd036d4ddb4ee8d8a4f31d4f2d1639
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-03-12 05:31:03 +0600

    Update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -28,7 +28,7 @@
             <div class="relative ml-3">
             <button id="profile-button" class="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
                 <span class="sr-only">Open user menu</span>
-                <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                <img class="size-8 rounded-full" src="https://via.placeholder.com/150" alt="">
             </button>
             <div id="profile-menu" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
                 <a href='/profile' class="block px-4 py-2 text-sm text-gray-700">Your Profile</a>
